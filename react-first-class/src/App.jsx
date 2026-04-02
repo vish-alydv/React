6a63a42f955b6bb74bc8ee4d42d751687ab7a94c
vish-alydv <thevishaleditor@gmail.com>
2026-04-02 10:36:36 +0530

    first React commit

---
+++ react-first-class/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const App = () => {
+  const value = 0;
+  return (
+    <>
+    <div></div>
+    <h1></h1>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ react-first-class/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      This is navbar
+    </div>
+  )
+}
+
+export default Navbar
